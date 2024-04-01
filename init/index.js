7c04b143624287ccf89b34537a4c39f4b27585e5
Harmanjot <iharmanjot08@gmail.com>
2024-04-01 11:42:08 -0400

    Add project files

---
+++ init/index.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+const MONGO_URL = "mongodb://127.0.0.1:27017/majorProject";
+
+main().then(() => {
+    console.log("connected to DB");
+}).catch((err) => {
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({ ...obj, owner: "65f4704ff059bb4cd621396f" }));
+    await Listing.insertMany(initData.data);
+    console.log("Data was initialized!");
+
+}
+
+initDB();
